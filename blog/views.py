edc5299e2045e10a4239aa14830ff5de317e330b
moboroboo <kalhoriali51@gmail.com>
2022-04-13 12:58:56 +0430

    created the blog app
    + made blog app and configured the urls.py and views.py

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def blog_view(request):
+    return render(request, 'blog/blog_list.html')
+
+def single_view(request):
+    return render(request, 'blog/blog_detail.html')
